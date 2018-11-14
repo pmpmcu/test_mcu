e33876999181ad79902f83331d3d675b072079cf
szt <pmp_mcu@163.com>
2018-11-14 11:03:28 +0800

    test

--- code/USER/main.c
+++ code/USER/main.c
@@ -71,6 +71,8 @@ void TIM3_DELAY_MS_(u16 times)
 	//uart_init(115200);	 //串口初始化为115200
 	uart1_init(115200);
 	printf("uart1_init_115200\r\n");
+	printf("uart1_init_115200\r\n");
+	printf("uart1_init_115200\r\n");
 	uart2_init(115200);
 	printf("uart2_init_115200\r\n");
 	USART_SendData(USART2, 99);
